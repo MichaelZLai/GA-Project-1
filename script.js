f0f85d0b93abf85c150d99e4e871e72e22163934
Michael Lai <mike24567@gmail.com>
2016-09-19 10:52:29 -0400

    Added psuedo code for hangman

---
+++ script.js
@@ -0,0 +1,9 @@
+//Initiate prompt to store word to be guessed
+
+//When user guesses letter, parse array of alphabet
+
+//If user gets a right letter, show letter on board else show body part
+
+//Prevent user from asking the same letter
+
+//Display alert stating you have won or lost! Play again?
